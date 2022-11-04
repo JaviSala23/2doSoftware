833121893d0bfa7d9bb098117b3b671b3dc7af74
JaviSala23 <javisala.ca@gmail.com>
2022-11-03 21:45:51 -0300

    kkk

--- Juego/principal.py
+++ Juego/principal.py
@@ -14,9 +14,10 @@ import pygame
  
 # Definimos algunos colores
 NEGRO = (0, 0 ,0)
+ROJO= (255, 0, 0)
 BLANCO = (255, 255, 255)
 VERDE = (0, 255, 0)
-ROJO = (255, 0, 0)
+VERDEROJO = (255, 0, 0)
 AZUL = (0, 0, 255)
 VIOLETA = (98, 0, 255)
 AMARILLO = (255,240,0)
@@ -36,6 +37,10 @@ hecho = False
 # Se usa para establecer cuan rápido se actualiza la pantalla
  
 reloj = pygame.time.Clock()
+
+movix=20
+moviy=20
+direccion="abajo"
   
 # -------- Bucle principal del Programa -----------
 while not hecho: #While hecho == False: o defininrlo While hecho != True: 
@@ -45,15 +50,40 @@ while not hecho: #While hecho == False: o defininrlo While hecho != True:
             hecho = True
         
     
-     
+    
     # --- LA LÓGICA DEL JUEGO DEBERÍA IR AQUÍ
   
     # --- EL CÓDIGO DE DIBUJO DEBERÍA IR AQUÍ
      
     # Primero, limpia la pantalla con blanco. No vayas a poner otros comandos de dibujo encima 
     # de esto, de otra forma serán borrados por este comando:
-    pantalla.fill(AZUL)
-     
+    
+    
+    #creo un rectangolo en la plantalla
+    if moviy<400 and direccion=="abajo":
+        pantalla.fill(VERDE)
+        pygame.draw.ellipse(pantalla, ROJO, [movix, moviy, 100, 100])
+        pygame.draw.rect(pantalla, ROJO, [movix+100, moviy+10, 100, 100])
+        pygame.draw.ellipse(pantalla, ROJO, [movix+200, moviy+20, 100, 100])
+        pygame.draw.rect(pantalla, ROJO, [movix+300, moviy+30, 100, 100])
+        pygame.draw.ellipse(pantalla, ROJO, [movix+400, moviy+50, 100, 100])
+        moviy=moviy+10
+    if moviy>=400:
+        direccion="arriba"
+    if moviy>0 and direccion=="arriba":
+        pantalla.fill(ROJO)
+        pygame.draw.rect(pantalla, VERDE, [movix, moviy+50, 100, 100])
+        pygame.draw.ellipse(pantalla, VERDE, [movix+100, moviy+30, 100, 100])
+        pygame.draw.rect(pantalla, VERDE, [movix+200, moviy+20, 100, 100])
+        pygame.draw.ellipse(pantalla, VERDE, [movix+300, moviy+10, 100, 100])
+        pygame.draw.rect(pantalla, VERDE, [movix+400, moviy, 100, 100])
+        moviy=moviy-10
+    if moviy<=0:
+        direccion="abajo"
+        
+    #movix=movix+10
+    
+    
     # --- Avanzamos y actualizamos la pantalla con lo que hemos dibujado.
     pygame.display.flip()
  
